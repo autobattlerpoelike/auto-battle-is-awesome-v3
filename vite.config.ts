e081f616cc9dab1a74be1f1a90a20b5ef4cddbb9
autobattlerpoelike <autobattlerpoelike@gmail.com>
2025-10-13 20:52:04 +0800

    Fix Vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: '/auto-battle-is-awesome-v3/', // 👈 must match your repo name exactly
 })
